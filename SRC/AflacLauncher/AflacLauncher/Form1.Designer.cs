2ea2ebe1f3969b653ab91e8a9843fbe725131f73
GlenDudley-E10150 <gdudley@aflac.com>
2020-09-23 17:41:30 -0400

    Added Timer Loop

---
+++ SRC/AflacLauncher/AflacLauncher/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace AflacLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 189);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "\r\nPlease Wait\r\nwhile we update \r\nClaims Payment System\r\n";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(479, 218);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aflac Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SRC/AflacLauncher/AflacLauncher/Form1.cs
@@ -0,0 +1,87 @@
+﻿using Aflac.Updater.Contract;
+using StreamJsonRpc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AflacLauncher
+{
+    public partial class Form1 : Form
+    {
+
+        bool WaitingTimeOutExpired = false;
+        bool AppIsUpdating = false;
+        string AppToStart = @"C:\Program Files (x86)\AflacApps\CPS\Aflac.ClaimsPaymentSystem.Shell.exe";
+        int TimeOutMax = 10;
+        int TimerCount = 0;
+
+
+        System.Windows.Forms.Timer myTimer = new Timer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+            myTimer.Interval = 1000;
+            myTimer.Start();
+        }
+
+
+        private void TimerEventProcessor(Object myObject, EventArgs myEventArgs)
+        {
+            System.Diagnostics.Debug.WriteLine(string.Format("Checking with Service Checkpoint : {0}",TimerCount));
+            _ = CheckAppAsync();
+            TimerCount += 1;
+
+            if (TimerCount >= TimeOutMax)
+            {
+                //Throw Error
+                myTimer.Stop();
+                MessageBox.Show("Error : Updating CPS...Please Try Again");
+                Application.Exit();
+            }
+            if (!AppIsUpdating)
+            {
+                LaunchApp();
+                Application.Exit();
+            }
+        }
+        private void LaunchApp()
+        {
+            System.Diagnostics.Process.Start(AppToStart);
+        }
+        private async Task CheckAppAsync()
+        {
+            //this.Show();
+            //Call  Service and Loop until timeout or service respond True
+            var pipeServer = new NamedPipeClientStream(".", "Aflac.AutoUpdator", PipeDirection.InOut, PipeOptions.Asynchronous);
+            await pipeServer.ConnectAsync();
+            var proxy = (IUpdateControler)JsonRpc.Attach(pipeServer).Attach(typeof(IUpdateControler));
+            //var result = await proxy.HeartBeatAsync(new HeartBeatCriteria() { Info = "Test" });
+            var result = await proxy.CheckAndUpdateAsync(new CheckAndUpdateCriteria() { Application = "CPS" });
+            if (result.State.Equals("Current"))
+            {
+                AppIsUpdating = false;                
+            }
+            else
+            {
+                AppIsUpdating = true;
+            }
+        }
+
+
+    }
+}
